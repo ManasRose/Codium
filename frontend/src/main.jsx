3d5dda6e7ac38505bd7c4e25889b8984b8d878b6
ManasRose <manasrose04@gmail.com>
2025-09-28 10:46:23 +0530

    Deploying frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./AuthContext.jsx";
-import ProjectRoutes from "./routes.jsx";
+import ProjectRoutes from "./Routes.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
