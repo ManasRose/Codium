fef19d00054deafb7306e7e105e4c31b484f4b06
ManasRose <manasrose04@gmail.com>
2026-03-25 16:36:30 +0530

    newAWS

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,8 +1,11 @@
 require("dotenv").config({ quiet: true });
 const { S3Client } = require("@aws-sdk/client-s3");
-const S3_BUCKET = process.env.S3_BUCKET || "manascodiumbucket";
-const REGION = process.env.AWS_REGION || "ap-south-1";
+
+const S3_BUCKET = process.env.S3_BUCKET || "codium-storage";
+const REGION = process.env.AWS_REGION || "eu-north-1";
+
 const s3 = new S3Client({
   region: REGION,
 });
+
 module.exports = { s3, S3_BUCKET };
